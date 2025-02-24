956defa76a6f897ab6270fdfc01fdf82a739aaee
Barkotnobi <salemsolomon1@gmail.com>
2025-02-23 21:25:38 -0600

    redeploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base:'/Netflix-clone2027/',
+  base:'/Netflix-clone2027/',
   plugins: [react()],
 })
